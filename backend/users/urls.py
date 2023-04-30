ae54013c526efd8e47db0de99187a920d9e14f3c
Иван Шамайский <shamaiskii.ivan@gmail.com>
2023-04-30 14:54:13 +0500

    add password reset

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,7 +2,10 @@ from django.contrib.auth.views import (
     LogoutView,
     LoginView,
     PasswordChangeView,
-    PasswordChangeDoneView
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
 )
 from django.urls import path
 
@@ -28,5 +31,17 @@ urlpatterns = [
     path('password_change/done/',
          PasswordChangeDoneView.as_view(
              template_name='users/password_change_done.html'),
-         name='password_change_done')
+         name='password_change_done'),
+    path('password_reset/',
+         PasswordResetView.as_view(
+             template_name='users/password_reset_form.html'),
+         name='password_reset_form'),
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm')
 ]
